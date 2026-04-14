4fc4c4ee6bfbfe627fc6674519c42747c2e05772
madpelsm <mathiasdepelsmaeker@gmail.com>
2026-04-15 01:15:46 +0200

    logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" type="image/png" sizes="512x512" href="/favicon.png" />
+	<link rel="icon" type="image/png" sizes="1254x1254" href="/logo.png" />
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 </svelte:head>
 
